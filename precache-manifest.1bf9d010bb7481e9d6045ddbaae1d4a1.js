95ef3793f60716be5d69ee33ecd9e9b1adc45824
seunghwijo <seunghwij@gmail.com>
2020-08-09 05:26:07 +0900

    Updates

---
+++ precache-manifest.1bf9d010bb7481e9d6045ddbaae1d4a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b69874785f0b3484ecf4ae1f943c2e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caeef67ab2c992ffed62",
+    "url": "/static/css/main.3b8de984.chunk.css"
+  },
+  {
+    "revision": "da91fca76777d8e65fc5",
+    "url": "/static/js/2.e2494b61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e2494b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caeef67ab2c992ffed62",
+    "url": "/static/js/main.f273f983.chunk.js"
+  },
+  {
+    "revision": "bd7f4f55b55e38cea2f1",
+    "url": "/static/js/runtime-main.58002af7.js"
+  }
+]);
